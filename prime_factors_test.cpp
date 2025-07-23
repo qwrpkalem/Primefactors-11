517c2781ec8fd2043ba08ee62c0e7020989d4036
jk <qwrpkalem@naver.com>
2025-07-23 09:55:51 +0900

    [feature] page 1

---
+++ prime_factors_test.cpp
@@ -0,0 +1,12 @@
+#include "gmock/gmock.h"
+#include "prime_factors.cpp"
+#include "vector"
+
+using std::vector;
+
+TEST(PrimeFactors, Of1)
+{
+	PrimeFactors prime_factor;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
+}
